ae07b69384661e2c9adc6b050d7c8fa5f8939ed4
romansen47 <romansen47@gmail.com>
2026-09-12 13:51:28 +0200

    Migrate engine settings to Jackson 3

--- src/main/java/demo/chess/api/service/EngineSettingsService.java
+++ src/main/java/demo/chess/api/service/EngineSettingsService.java
@@ -16,7 +16,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 
 import demo.chess.api.dto.EngineConfigOverviewDto;
 import demo.chess.api.dto.EngineConfigStoreDto;
