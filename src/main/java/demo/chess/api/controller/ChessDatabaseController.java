223e668acdcc81b8475e3afc679dcc64c0dcccdc
romansen47 <romansen47@gmail.com>
2026-09-14 22:41:21 +0200

    Clear shared analysis state when loading database game

--- src/main/java/demo/chess/api/controller/ChessDatabaseController.java
+++ src/main/java/demo/chess/api/controller/ChessDatabaseController.java
@@ -148,7 +148,7 @@ public class ChessDatabaseController {
             value = "/games/{gameId}/load",
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> loadGame(@PathVariable long gameId) {
-        analysisReplayService.cancel();
+        analysisReplayService.clear();
 
         try {
             UciGameDto game = chessDatabaseService.loadGame(gameId);
